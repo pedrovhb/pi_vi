58ade92a53751a0a73c51cf5cca69c4fc6a79f07
Feliciano <feliciano@localhost.localdomain>
2019-05-22 09:08:29 -0400

    Commit inicial

---
+++ main.py
@@ -0,0 +1,132 @@
+# -*- coding: UTF-8 -*-
+
+from flask import Flask, Response, jsonify, request
+import json
+import inspect
+import linuxcnc
+
+app = Flask(__name__)
+
+s = linuxcnc.stat()
+c = linuxcnc.command()
+
+
+def ok_for_mdi():
+    s.poll()
+    return not s.estop and s.enabled and s.homed \
+           and (s.interp_state == linuxcnc.INTERP_IDLE)
+
+
+# Info Linux CNC:
+# http://linuxcnc.org/docs/2.6/html/common/python-interface.html
+
+
+#########################################################
+# Info
+
+@app.route("/axis_positions")
+def get_axis_positions():
+    s.poll()
+    axes = s.axis[:3]
+    axes_positions = [round(axis['output'], 4) for axis in axes]
+    axes_positions = {
+        'X': axes_positions[0],
+        'Y': axes_positions[1],
+        'Z': axes_positions[2]}
+    return jsonify(axes_positions)
+
+
+@app.route("/status")
+def get_status():
+    s.poll()
+    props = inspect.getmembers(s, lambda a:not(inspect.isroutine(a)))
+    props_dict = {}
+    for p in props:
+        key, val = p[0], p[1]
+        props_dict[key] = val
+    props_dict = json.dumps(props_dict, default=lambda x: repr(x))
+    return Response(props_dict, 200, mimetype='application/json')
+
+
+#########################################################
+# Movimentação
+
+
+@app.route("/jog", methods=['POST'])
+def jog():
+    if not ok_for_mdi():
+        return jsonify({'status': 'busy'})
+    
+    data = request.get_json()
+    print(data)
+    axis = data.get('axis')
+    speed = data.get('speed')
+    increment = data.get('increment')
+    c.jog(linuxcnc.JOG_INCREMENT, axis, speed, increment)
+    return jsonify({'status': 'ok'})
+
+
+@app.route("/mdi", methods=['POST'])
+def mdi():
+    if not ok_for_mdi():
+        return jsonify({'status': 'busy'})
+    print(request.headers)
+    data = request.get_json()
+    print(data)
+    mdi_command = data.get('mdi')
+    c.mode(linuxcnc.MODE_MDI)
+    c.mdi(mdi_command)
+    return jsonify({'status': 'ok'})
+
+
+@app.route("/home", methods=['POST'])
+def home():
+    if ok_for_mdi():
+        c.home(0)
+        c.home(1)
+        c.home(2)
+        return jsonify({'status': 'ok'})
+    return jsonify({'status': 'busy'})
+
+
+#########################################################
+# Emergência
+
+@app.route("/estop", methods=["POST", "GET"])
+def state_estop():
+    c.state(linuxcnc.STATE_ESTOP)
+    return jsonify({'status': 'ok'})
+
+
+@app.route("/estop_reset", methods=["POST"])
+def state_estop_reset():
+    c.state(linuxcnc.STATE_ESTOP_RESET)
+    return jsonify({'status': 'ok'})
+
+
+#########################################################
+# On/Off
+
+@app.route("/state_on", methods=["POST", "GET"])
+def state_on():
+    c.state(linuxcnc.STATE_ON)
+    return jsonify({'status': 'ok'})
+
+
+@app.route("/state_off", methods=["POST"])
+def state_off():
+    c.state(linuxcnc.STATE_OFF)
+    return jsonify({'status': 'ok'})
+
+
+#########################################################
+
+@app.after_request
+def after_request(response):
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Headers'] = '*'
+    return response
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0')
---
+++ testes.py
@@ -0,0 +1,79 @@
+# -*- coding: UTF-8 -*-
+
+import requests
+
+host = 'http://localhost:5000'
+
+
+def teste_home():
+    print('=====================================')
+    print('Testando sequência de homing')
+    r = requests.post('%s/home' % host)
+    print(r.json())
+
+
+def teste_jog():
+    print('=====================================')
+    print('Testando sequência de jogging')
+    
+    print('X:')
+    r0 = requests.post('%s/jog' % host, json={
+        'axis': 0, 'speed': 10, 'increment': -50})
+    print(r0.json())
+    
+    print('Y:')
+    r1 = requests.post('%s/jog' % host, json={
+        'axis': 1, 'speed': 10, 'increment': -50})
+    print(r1.json())
+
+    print('Z:')
+    r2 = requests.post('%s/jog' % host, json={
+        'axis': 2, 'speed': 10, 'increment': -50})
+    print(r2.json())
+
+
+def teste_estop():
+    print('=====================================')
+    print('Testando parada de emergência')
+    r = requests.post('%s/estop' % host)
+    print(r.json())
+    
+
+def teste_estop_reset():
+    print('=====================================')
+    print('Testando reset de parada de emergência')
+    r = requests.post('%s/estop_reset' % host)
+    print(r.json())
+
+
+def teste_state_on():
+    print('=====================================')
+    print('Testando state on')
+    r = requests.post('%s/state_on' % host)
+    print(r.json())
+    
+
+def teste_state_off():
+    print('=====================================')
+    print('Testando state off')
+    r = requests.post('%s/state_off' % host)
+    print(r.json())
+
+
+def teste_axis_positions():
+    print('=====================================')
+    print('Testando posições de eixos')
+    r = requests.post('%s/axis_positions' % host)
+    print(r.json())
+
+
+def teste_state_off():
+    print('=====================================')
+    print('Testando state off')
+    r = requests.post('%s/state_off' % host)
+    print(r.json())
+
+
+
+
+
